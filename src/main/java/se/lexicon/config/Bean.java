be82f29d377e723cb63c3ea5f5f05dd66c7c0fb1
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-09-11 11:42:48 +0200

    Created

---
+++ src/main/java/se/lexicon/config/Bean.java
@@ -0,0 +1,12 @@
+package se.lexicon.config;
+
+import java.util.Scanner;
+
+public class Bean {
+
+    Scanner scanner;
+
+    public Bean(Scanner scanner) {
+        this.scanner = scanner;
+    }
+}
