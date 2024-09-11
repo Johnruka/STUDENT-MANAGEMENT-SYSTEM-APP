5784b35dce2f3ed4a04be56cb382b5bee1e38100
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-09-11 10:50:26 +0200

    @Bean

--- src/main/java/se/lexicon/config/ComponentScanConfig.java
+++ src/main/java/se/lexicon/config/ComponentScanConfig.java
@@ -1,9 +1,19 @@
 package se.lexicon.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import se.lexicon.data_access.Impl.StudentDaoListImpl;
+import se.lexicon.util.ScannerInputService;
 
 @Configuration
 @ComponentScan(basePackages = "se.lexicon")
 public class ComponentScanConfig {
-}
+
+    @Bean
+    public StudentDaoListImpl StudentDao() {
+        return new StudentDaoListImpl();
+    }
+
+    }
+
