991df90d3b93f2538a791b48405f2444042d2ba2
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-09-11 10:24:10 +0200

    @Autowired dependency

--- src/main/java/se/lexicon/Service/StudentManagementConsoleImpl.java
+++ src/main/java/se/lexicon/Service/StudentManagementConsoleImpl.java
@@ -1,4 +1,50 @@
 package se.lexicon.Service;
 
-public class StudentManagementConsoleImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import se.lexicon.data_access.dao.StudentDao;
+import se.lexicon.model.Student;
+import se.lexicon.util.userInputServiceDao;
+
+import java.util.List;
+
+public class StudentManagementConsoleImpl implements StudentManagementDao {
+
+    private userInputServiceDao ScannerService;
+    private StudentDao studentDao;
+
+   @Autowired
+    public StudentManagementConsoleImpl(userInputServiceDao scannerService, StudentDao studentDao) {
+        ScannerService = scannerService;
+        this.studentDao = studentDao;
+    }
+
+    @Override
+    public Student create() {
+        return null;
+    }
+
+    @Override
+    public Student save(Student student) {
+        return null;
+    }
+
+    @Override
+    public Student find(int id) {
+        return null;
+    }
+
+    @Override
+    public Student remove(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Student> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public Student edit(Student student) {
+        return null;
+    }
 }
