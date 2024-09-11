2cb5bef1780ffbb8ff03be1e0019f82a7d50e783
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-09-11 09:35:37 +0200

    Implemented

--- src/main/java/se/lexicon/data_access/Impl/StudentDaoListImpl.java
+++ src/main/java/se/lexicon/data_access/Impl/StudentDaoListImpl.java
@@ -1,4 +1,37 @@
 package se.lexicon.data_access.Impl;
 
-public class StudentDaoListImpl {
+import se.lexicon.data_access.dao.StudentDao;
+import se.lexicon.model.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentDaoListImpl implements StudentDao {
+
+    private List<Student> students;
+
+    public StudentDaoListImpl() {
+        this.students = new ArrayList<>();
+
+    }
+
+    @Override
+    public Student save(Student student) {
+        return null;
+    }
+
+    @Override
+    public Student Find(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Student> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public Void delete(int id) {
+        return null;
+    }
 }
