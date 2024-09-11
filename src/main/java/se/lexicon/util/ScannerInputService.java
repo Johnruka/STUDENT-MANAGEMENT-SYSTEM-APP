1c7f38628ef24d474a9f8003fa39b337481f05ec
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-09-11 10:18:15 +0200

    @Autowired dependency

--- src/main/java/se/lexicon/util/ScannerInputService.java
+++ src/main/java/se/lexicon/util/ScannerInputService.java
@@ -1,10 +1,16 @@
 package se.lexicon.util;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import java.util.Scanner;
 
 public class ScannerInputService implements userInputServiceDao {
 
     Scanner scanner;
+@Autowired
+    public ScannerInputService(Scanner scanner) {
+        this.scanner = scanner;
+    }
 
     @Override
     public String getString() {
