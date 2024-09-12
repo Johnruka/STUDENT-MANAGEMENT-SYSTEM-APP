446ec0e444e5fa42760d502307fb0e8e21492588
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-09-12 10:47:34 +0200

    amodified and implemented

--- src/main/java/se/lexicon/data_access/dao/StudentDao.java
+++ src/main/java/se/lexicon/data_access/dao/StudentDao.java
@@ -1,13 +1,24 @@
 package se.lexicon.data_access.dao;
 
+
 import se.lexicon.model.Student;
 
 import java.util.List;
 
 public interface StudentDao {
 
-    Student save (Student student);
-    static Student Find(int id);
-    List<Student>findAll();
+    Student save(Student student);
+
+    static Student Find(int id) {
+        return null;
+    }
+
+
+    Student Find(int id);
+
+    List<Student> findAll();
+
     Void delete(int id);
 }
+
+
