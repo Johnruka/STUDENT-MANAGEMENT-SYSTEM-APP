64057bedc23855b170cc9635d9a05f9071f6b11e
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-09-12 09:59:18 +0200

    Implemented

--- src/main/java/se/lexicon/data_access/Impl/StudentDaoListImpl.java
+++ src/main/java/se/lexicon/data_access/Impl/StudentDaoListImpl.java
@@ -4,34 +4,53 @@ import se.lexicon.data_access.dao.StudentDao;
 import se.lexicon.model.Student;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
 
 public class StudentDaoListImpl implements StudentDao {
 
-    private List<Student> students;
+    List<Student> studentsStorage;
 
     public StudentDaoListImpl() {
-        this.students = new ArrayList<>();
+        System.out.println("Student list initiated...");
+        this.studentsStorage = new ArrayList<>();
 
     }
 
     @Override
     public Student save(Student student) {
-        return null;
+        studentsStorage.add(student);
+        return student;
+
     }
 
     @Override
     public Student Find(int id) {
-        return null;
+        Optional<Student> studentOptional = studentsStorage.stream()
+                .filter(student -> student.getId() == id)
+                .findFirst();
+        return studentOptional.orElse(null);
+
     }
 
     @Override
     public List<Student> findAll() {
-        return List.of();
+        return new ArrayList<>(studentsStorage);
+
     }
 
     @Override
     public Void delete(int id) {
+        Iterator<Student> iterator = studentsStorage.iterator();
+        while(iterator.hasNext()) {
+            Student removeStudent = iterator.next();
+            if(removeStudent.getId() == id) {
+                iterator.remove();
+                break;
+            }
+        }
+
         return null;
     }
 }
